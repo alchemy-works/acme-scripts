9b260ad783d5ab2820a02203cab3623141119a49
cbdyzj <cbdyzj@jianzhao.org>
2019-09-21 16:50:05 +0800

    Add update script

--- update_cert.sh
+++ update_cert.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-# todo
+
+DOMAIN=$1
+
+if [ -z "$str" ]; then
+  echo 'Domain required'
+  exit -1
+fi
+
+if [ ! -f "~/dp-acme/$DOMAIN/$DOMAIN.cer" ]; then
+    echo "File not exist: ~/dp-acme/$DOMAIN/$DOMAIN.cer"
+    exit -1
+fi
+
+if [ ! -f "~/dp-acme/$DOMAIN/$DOMAIN.key" ]; then
+    echo "File not exist: ~/dp-acme/$DOMAIN/$DOMAIN.key"
+    exit -1
+fi
+
+mkdir -p /etc/ssl/cert/$DOMAIN
+cp "~/dp-acme/$DOMAIN/$DOMAIN.cer" "/etc/ssl/cert/$DOMAIN"
+cp "~/dp-acme/$DOMAIN/$DOMAIN.key" "/etc/ssl/cert/$DOMAIN"
